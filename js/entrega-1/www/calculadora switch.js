62ba07cbf185dece87a75f75c2759d33eec48499
hab13 <bgonzza@gmail.com>
2020-03-10 10:50:24 +0100

    Ejercicio de calculadora switch

--- js/entrega-1/www/calculadora switch.js
+++ js/entrega-1/www/calculadora switch.js
@@ -1,17 +1,29 @@
 "use string";
 
-let number1 = prompt("introduce primer numero");
-let number2 = prompt("introduce segundo numero ");
-let operation = prompt("introduce operacion");
+let number1 = prompt("introduce un numero");
+let number2 = prompt("introduce un segundo numero");
+let operation = prompt("introduce una  operacion");
 
-
-switch  {
-    case(operation=== "+")
-     console.log(number1 + number2)
-break;
-    case (operation === "-")
-console.log(number1 - number2)
-
-    default:
-        break;
+switch (operation) {
+  case "+":
+    console.log(number1 + number2);
+    break;
+  case "-":
+    console.log(number1 - number2);
+    break;
+  case "*":
+    console.log(number1 * number2);
+    break;
+  case "**":
+    console.log(number1 ** number2);
+    break;
+  case "/":
+  case number2 !== 0:
+    console.log(number1 / number2);
+    break;
+  case number2 === 0:
+    console.log("No dividas entre 0 que da infinito");
+    break;
+  default:
+    console.log("Error");
 }
